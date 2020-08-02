2ace11f4d253fddd02f90280df6ce5fef321adde
hyein <hyeinjung99@naver.com>
2020-08-03 04:33:48 +0900

    비밀설정

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hyeinjung:hi990916@node.ukxyr.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -11,8 +20,21 @@ mongoose.connect('mongodb+srv://hyeinjung:hi990916@node.ukxyr.mongodb.net/<dbnam
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!안녕바위')
 })
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
